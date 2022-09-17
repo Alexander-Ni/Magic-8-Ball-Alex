79e3b41e6f6ebfd42a4745c6b08cd85b1c729406
Alexander Nielsen <nielsen.alexander1@gmail.com>
2022-09-17 20:23:39 +0200

    Initial Commit

---
+++ Magic-8-Ball-Alex/ViewController.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let ballArray = [#imageLiteral(resourceName: "ball1"), #imageLiteral(resourceName: "ball2"), #imageLiteral(resourceName: "ball3"), #imageLiteral(resourceName: "ball4"), #imageLiteral(resourceName: "ball5") ]
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    @IBAction func ButtonPressed(_ sender: UIButton) {
+        imageView.image = ballArray.randomElement()
+    }
+    
+    }
